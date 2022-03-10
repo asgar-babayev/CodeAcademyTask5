966204e383b2e7e0b781d5c883290c345ea81cb9
[Asgar Babayev] <[tu7609td3@code.edu.az]>
2022-03-11 01:43:44 +0400

    updated task

--- ClassTask/ClassTask/Program.cs
+++ ClassTask/ClassTask/Program.cs
@@ -11,13 +11,7 @@ namespace ClassTask
             Console.WriteLine("Maşın üçün çıxarılan nəticə...");
             Car car = new Car()
             {
-                color = "Qara",
-                model = "BMW",
-                cycle = 4,
-                passangerCount = 4,
-                motor = 2.5,
-                driveWay = 100,
-                driveTime = 2
+                color = "Qara", model = "BMW", cycle = 4, passangerCount = 4, motor = 2.5, driveWay = 100, driveTime = 2
             };
             car.Print();
             car.GetSpeed();
@@ -25,13 +19,7 @@ namespace ClassTask
             Console.WriteLine("Avtobus üçün çıxarılan nəticə...");
             Bus bus = new Bus()
             {
-                color = "Qirmizi",
-                model = "Daewoo",
-                cycle = 6,
-                passangerCount = 20,
-                motor = 2.2,
-                driveWay = 120,
-                driveTime = 4
+                color = "Qirmizi", model = "Daewoo", cycle = 6, passangerCount = 20, motor = 2.2, driveWay = 120, driveTime = 4
             };
             bus.Print();
             bus.GetSpeed();
@@ -39,12 +27,7 @@ namespace ClassTask
             Console.WriteLine("Velosiped üçün çıxarılan nəticə...");
             Bicycle bicycle = new Bicycle()
             {
-                color = "Göy",
-                model = "Road Bike",
-                cycle = 2,
-                passangerCount = 1,
-                driveWay = 30,
-                driveTime = 2
+                color = "Göy", model = "Road Bike", cycle = 2, passangerCount = 1, driveWay = 30, driveTime = 2
             };
             bicycle.Print();
             bicycle.GetSpeed();
